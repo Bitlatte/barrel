e347a9cc3bdb769fa45c720af09ef2b1f4324dd7
bitlatte <github@bitlatte.io>
2022-06-29 23:18:52 -0500

    Add Optional Placeholder to String Modeler

--- src/lib/modeling/string-modeler.svelte
+++ src/lib/modeling/string-modeler.svelte
@@ -5,6 +5,6 @@
 <div>
 	<div class={'input-group'}>
 		<span>Value</span>
-		<input bind:value class={'input focus:outline-none'} />
+		<input bind:value class={'input focus:outline-none'} placeholder={'(Optional)'} />
 	</div>
 </div>
