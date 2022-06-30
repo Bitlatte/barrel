4a408aac3b78ff5cd9e52f946f943cda637993e6
bitlatte <github@bitlatte.io>
2022-06-30 00:25:36 -0500

    FIX: Removed Unnecessary Code

--- src/lib/modeling/model.svelte
+++ src/lib/modeling/model.svelte
@@ -4,28 +4,26 @@
 	import DocumentModeler from '$lib/modeling/document-modeler.svelte';
 </script>
 
-<Section large>
-	<div class={'mockup-window bg-base-300 shadow-md md:min-w-[750px]'}>
-		<div class={'p-4 bg-base-200 overflow-hidden'}>
-			<div class={'text-sm breadcrumbs'}>
-				<ul>
-					<li>
-						<div class={'breadcrumbs'}>
-							<ul>
-								<li>
-									<Editable placeholder={'Collection'} />
-								</li>
-								<li>
-									<Editable placeholder={'Document'} uuid />
-								</li>
-							</ul>
-						</div>
-					</li>
-				</ul>
-			</div>
-			<div>
-				<DocumentModeler />
-			</div>
+<div class={'mockup-window bg-base-300 shadow-md md:min-w-[750px]'}>
+	<div class={'p-4 bg-base-200 overflow-hidden'}>
+		<div class={'text-sm breadcrumbs'}>
+			<ul>
+				<li>
+					<div class={'breadcrumbs'}>
+						<ul>
+							<li>
+								<Editable placeholder={'Collection'} />
+							</li>
+							<li>
+								<Editable placeholder={'Document'} uuid />
+							</li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div>
+			<DocumentModeler />
 		</div>
 	</div>
-</Section>
+</div>
